681fe2b639e511334033cfbb3bda8a9f400d23ed
Dan Martin <dhmartin27@hotmail.com>
2022-12-28 15:45:56 -0500

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/sh -v
 
 echo "Executing command..."
-printenv GITHUB_WORKSPACE
+#printenv GITHUB_WORKSPACE
 echo "curr dir"
 pwd
 ls
-sh -c "$*"
-exit_code=$?
-
-echo "Done."
-exit $exit_code
+#sh -c "$*"
+#exit_code=$?
+#echo "Done."
+#exit $exit_code
